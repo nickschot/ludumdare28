1db86f233fca0a691b5636ce78c2e155e7167eaf
Nick Schot <nickschot@gmail.com>
2013-12-14 11:45:11 +0100

    Added main class

---
+++ Ludum Dare 28/public_html/js/main.js
@@ -0,0 +1,21 @@
+//Initialize and build form based on backend info
+var m;
+
+window.addEvents({
+	'load' : function() {
+        
+    },
+	'domready': function() {
+		//config
+		Locale.use('en-US');
+
+		//start
+		m = new Main();
+	}
+});
+
+var Main = new Class({
+    initialize: function(){
+        
+    }
+});
