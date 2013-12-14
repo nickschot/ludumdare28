2b6321a13eab3d24fcc40630c5a16e50430aa680
windwarrior <lennartbuit@gmail.com>
2013-12-14 13:02:59 +0100

    Push

--- Ludum Dare 28/public_html/js/main.js
+++ Ludum Dare 28/public_html/js/main.js
@@ -18,15 +18,43 @@ window.addEvents({
 
 var Main = new Class({
     initialize: function(){
-        requestAnimationFrame(render);
+        this.nextTime = 0;
+        this.stateManager = undefined; //TODO: actually make a stateManager
+        this.ticks = 30;
+
+        //PIXI shizzle
+        this.stage = new PIXI.Stage(0x66FF99);
+        this.renderer = new PIXI.autoDetectRenderer(800, 600);
+
+        //Stats
+        this.updateStats = new Stats();
+        this.frameStats = new Stats();
+
         update();        
+        requestAnimationFrame(render);
+        
+        $$(document.body).adopt({this.updateStats.domElement, this.frameStats.domElement});
     },
 
     render: function() {
-
+        this.frameStats.begin();
+        this.stateManager.render();
+        this.frameStats.end();
     },
     
     update: function() {
-
+        this.updateStats.begin();
+        // Burn some cycles till the next update is requested
+        while((new Date).getTime() < next_time){}
+        // first ask the time
+        var time = (new Date).getTime();
+        // then update 
+        this.stateManager.update();    
+        // then set next_time and call ourself with interval
+        this.next_time = time + (1000 / this.ticks);
+        // update our stats
+        this.updateStats.end();
+        // then callback ourself with some marge, in this case 10ms
+        window.setTimeout(update, ((this.next_time - time) - 5));
     }
 });
