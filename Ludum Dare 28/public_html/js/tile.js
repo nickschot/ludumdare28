da20593fa72b6a204ed02c1fa15a35660014440b
Nick Schot <nickschot@gmail.com>
2013-12-14 12:20:46 +0100

    -Added Tiletypes with three base types: 	*Grass 	*Tree 	*Wall (with orientation) -Added implementation for tiletypes in tile

--- Ludum Dare 28/public_html/js/tile.js
+++ Ludum Dare 28/public_html/js/tile.js
@@ -1,5 +1,28 @@
 var Tile = new Class({
-    initialize: function(){
-        
+    initialize: function(type, x, y, orientation){
+        this.x = x;
+        this.y = y;
+        this.size = 32;
+        this.tile = this.getTileType(type);
+        this.orientation = orientation !== undefined ? orientation : 'top';
+    },
+    
+    getTileType: function(type){
+        var tileType;
+        switch(type){
+            case 'grass':
+                tileType = new GrassTile(this.size);
+                break;
+            case 'tree':
+                tileType = new TreeTile(this.size);
+            case 'wall':
+                tileType = new WallTile(this.size, this.orientation);
+            default:
+                tileType = false;
+                break;
+        }
+        return tileType;
     }
+    
+    
 });
