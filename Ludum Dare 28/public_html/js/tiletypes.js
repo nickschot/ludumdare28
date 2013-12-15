9e773e1f64c42e4068ddbdd7063fd19f466d63e5
Nick Schot <nickschot@gmail.com>
2013-12-15 13:54:14 +0100

    Removed spritesheet from tiletypes

--- Ludum Dare 28/public_html/js/tiletypes.js
+++ Ludum Dare 28/public_html/js/tiletypes.js
@@ -2,63 +2,54 @@ var tileTypes = {
     '00ff00': {
         'type': 'grass',
         'wall': false,
-        'spritesheet': 'img/game/spritesheet.png',
         'x': 1,
         'y': 3
     },
     '000000': {
         'type': 'mountain',
         'wall': true,
-        'spritesheet': 'img/game/spritesheet.png',
         'x': 0,
         'y': 0
     },
     'ffff00': {
         'type': 'path_vertical',
         'wall': false,
-        'spritesheet': 'img/game/spritesheet.png',
         'x': 2,
         'y': 1
     },
     'ff8800': {
         'type': 'path_horizontal',
         'wall': false,
-        'spritesheet': 'img/game/spritesheet.png',
         'x': 2,
         'y': 0
     },
     'ffbb00': {
         'type': 'path_intersection',
         'wall': false,
-        'spritesheet': 'img/game/spritesheet.png',
         'x': 0,
         'y': 3
     },
     'ffbb44': {
         'type': 'path_corner_south_east',
         'wall': false,
-        'spritesheet': 'img/game/spritesheet.png',
         'x': 0,
         'y': 0
     },
     'ffbb88': {
         'type': 'path_corner_west_south',
         'wall': false,
-        'spritesheet': 'img/game/spritesheet.png',
         'x': 1,
         'y': 0
     },
     'ffbbcc': {
         'type': 'corner_south_north_west',
         'wall': false,
-        'spritesheet': 'img/game/spritesheet.png',
         'x': 1,
         'y': 1
     },
     'ffbbff': {
         'type': 'corner_south_east_north',
         'wall': false,
-        'spritesheet': 'img/game/spritesheet.png',
         'x': 0,
         'y': 1
     },
